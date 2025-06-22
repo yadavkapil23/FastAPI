294a186437609a2780dd867d6870c0937e830a62
Kapil <kydv901@gmail.com>
2025-06-22 17:04:02 +0530

    added the http exception and path function

--- patient.py
+++ patient.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
 import json
 app = FastAPI()
 
@@ -20,3 +20,12 @@ def about():
 def view():
     data = load_data()
     return data
+
+@app.get("/patient/{patient_id}")
+def view_patient(patient_id : str = Path(...,description='ID of patient in DB',example='P001')):
+    
+    info = load_data()
+
+    if patient_id in info:
+        return info[patient_id]
+    return {'error' : 'Patient not found.'}
